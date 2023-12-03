ed782026e734b69ec1d57b32c1252ad0d80d4187
agentsim <2792224+agentsim@users.noreply.github.com>
2023-12-02 21:41:38 -0500

    Day 2 solutions

---
+++ src/aoc/src/aoc2/mod.rs
@@ -0,0 +1,210 @@
+use anyhow::{bail, Result};
+use std::str::Chars;
+use std::iter::Peekable;
+
+static INPUT: &str = include_str!("input");
+
+enum Colour {
+    Red,
+    Green,
+    Blue
+}
+
+fn skip(chars: &mut Peekable<Chars>, mut n: usize) {
+    while n > 0 && chars.next().is_some() {
+        n = n - 1
+    }
+}
+
+fn skip_whitespace(chars: &mut Peekable<Chars>) {
+    while let Some(ch) = chars.peek() {
+        if ch.is_whitespace() {
+            chars.next();
+        } else {
+            break
+        }
+    }
+}
+
+fn read_colour(chars: &mut Peekable<Chars>) -> Result<Colour> {
+    if let Some(ch) = chars.next() {
+        if ch == 'r' {
+            skip(chars, 2);
+            Ok(Colour::Red)
+        } else if ch == 'g' {
+            skip(chars, 4);
+            Ok(Colour::Green)
+        } else if ch == 'b' {
+            skip(chars, 3);
+            Ok(Colour::Blue)
+        } else {
+            bail!("Unexpected char while reading colour: {}", ch);
+        }
+    } else {
+        bail!("Unexpected end of input while reading colour");
+    }
+}
+
+fn read_number(chars: &mut Peekable<Chars>) -> u32 {
+    let mut num = 0;
+
+    loop {
+        if let Some(ch) = chars.next() {
+            if let Some(v) = ch.to_digit(10) {
+                num = num * 10 + v;
+            } else {
+                break
+            }
+        } else {
+            break
+        }
+    }
+
+    num
+}
+
+fn parse_result(chars: &mut Peekable<Chars>) -> Result<(u32, Colour)> {
+    let number = read_number(chars);
+
+    skip_whitespace(chars);
+
+    let colour = read_colour(chars)?;
+
+    Ok((number, colour))
+}
+
+fn parse_draw(chars: &mut Peekable<Chars>) -> Result<(u32, u32, u32)> {
+    let mut rc = (0, 0, 0);
+    let (number, colour) = parse_result(chars)?;
+
+    match colour {
+        Colour::Red => rc.0 = number,
+        Colour::Green => rc.1 = number,
+        Colour::Blue => rc.2 = number,
+    }
+
+    skip_whitespace(chars);
+
+    if let Some(ch) = chars.next() {
+        if ch == ';' {
+            return Ok(rc)
+        }
+    } else {
+        return Ok(rc)
+    }
+
+    skip_whitespace(chars);
+
+    let (number, colour) = parse_result(chars)?;
+
+    match colour {
+        Colour::Red => rc.0 = number,
+        Colour::Green => rc.1 = number,
+        Colour::Blue => rc.2 = number,
+    }
+
+    skip_whitespace(chars);
+
+    if let Some(ch) = chars.next() {
+        if ch == ';' {
+            return Ok(rc)
+        }
+    } else {
+        return Ok(rc)
+    }
+
+    skip_whitespace(chars);
+
+    let (number, colour) = parse_result(chars)?;
+
+    match colour {
+        Colour::Red => rc.0 = number,
+        Colour::Green => rc.1 = number,
+        Colour::Blue => rc.2 = number,
+    }
+
+    skip_whitespace(chars);
+
+    if let Some(ch) = chars.peek() {
+        if *ch == ';' {
+            chars.next();
+        }
+    }
+
+    Ok(rc)
+}
+
+fn parse_game(input: &str) -> Result<(bool, u32)> {
+    let mut chars = input.chars().peekable();
+
+    skip(&mut chars, 5);
+    let game_id = read_number(&mut chars);
+
+    skip_whitespace(&mut chars);
+
+    while chars.peek().is_some() {
+        let (r, g, b) = parse_draw(&mut chars)?;
+
+        if r > 12 || g > 13 || b > 14 {
+            return Ok((false, game_id))
+        }
+
+        skip_whitespace(&mut chars);
+    }
+
+    Ok((true, game_id))
+}
+
+fn parse_game_2(input: &str) -> Result<(u32)> {
+    let mut chars = input.chars().peekable();
+
+    skip(&mut chars, 5);
+    let game_id = read_number(&mut chars);
+    let mut max = (0, 0, 0);
+
+    skip_whitespace(&mut chars);
+
+    while chars.peek().is_some() {
+        let (r, g, b) = parse_draw(&mut chars)?;
+
+        if r > max.0 {
+            max.0 = r
+        }
+
+        if g > max.1 {
+            max.1 = g
+        }
+
+        if b > max.2 {
+            max.2 = b
+        }
+
+        skip_whitespace(&mut chars);
+    }
+
+    Ok(max.0 * max.1 * max.2)
+}
+
+pub fn solve1() -> Result<u32> {
+    let mut rc = 0;
+
+    for line in INPUT.lines() {
+        let (possible, id) = parse_game(line)?;
+
+        if possible {
+            rc = rc + id;
+        }
+    }
+
+    Ok(rc)
+}
+
+pub fn solve2() -> Result<u32> {
+    let mut rc = 0;
+
+    for line in INPUT.lines() {
+        rc = rc + parse_game_2(line)?;
+    }
+
+    Ok(rc)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate aoc;
 
+use anyhow::Result;
 use aoc::*;
 
-fn main() {
+fn main() -> Result<()> {
     let sol1 = aoc1::solve1();
     let sol2 = aoc1::solve2();
 
     println!("AOC 1.1 - {}", sol1);
     println!("AOC 1.2 - {}", sol2);
+
+    let sol2_1 = aoc2::solve1()?;
+    let sol2_2 = aoc2::solve2()?;
+
+    println!("AOC 2.1 - {}", sol2_1);
+    println!("AOC 2.2 - {}", sol2_2);
+    Ok(())
 }
